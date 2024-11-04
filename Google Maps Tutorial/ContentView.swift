f28b0d10bc1ea7b90767128bd13da3fc99cc3eb6
Fatih Durmaz <fatih.durmaz@outlook.com>
2024-11-05 00:47:45 +0300

    Initial Commit

---
+++ Google Maps Tutorial/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Google Maps Tutorial
+//
+//  Created by Fatih Durmaz on 5.11.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Google Maps Tutorial/Google_Maps_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Google_Maps_TutorialApp.swift
+//  Google Maps Tutorial
+//
+//  Created by Fatih Durmaz on 5.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct Google_Maps_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
